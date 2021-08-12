7bcfa529c5ec9c3d7e469bb3474f5949b6cc0e6d
Oliver Frolovs <oliver@frolovs.me>
2021-08-12 03:26:29 +0100

    Add Cloud NAT Gateway to facilitate nodes access to Docker Hub
    Signed-off-by: Oliver Frolovs <oliver@frolovs.me>

--- main.tf
+++ main.tf
@@ -105,3 +105,19 @@ resource "google_compute_router" "router" {
   project     = var.project
   description = "Provides access to Docker Hub to the K8s nodes"
 }
+
+# TODO investigate nat_ips = [] and drain_nat_ips = []
+
+resource "google_compute_router_nat" "nat" {
+  name    = "k8s-nodes-nat"
+  router  = google_compute_router.router.name
+  region  = google_compute_router.router.region
+
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+
+  subnetwork {
+    name                    = google_compute_subnetwork.prod.self_link
+    source_ip_ranges_to_nat = ["PRIMARY_IP_RANGE"]
+  }
+}
