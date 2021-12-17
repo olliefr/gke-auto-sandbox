64c2c5a992e697c3d815490cdb50fe2c6b4a2903
Oliver Frolovs <oliver@frolovs.me>
2021-12-17 12:32:40 +0000

    update required providers and terraform versions

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 terraform {
-  required_version = ">= 0.15"
+  required_version = "~> 1.0.10"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.4"
+    }
+  }
 }
 
 provider "google" {
