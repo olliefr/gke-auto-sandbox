93b8fc69f70fa276103f5ae3973ec042b34ec58e
Oliver Frolovs <oliver@devilmicelabs.com>
2023-08-19 16:04:03 +0100

    Massive upgrade!
    * Latest Terraform and provider versions.
    * Pin provider versions strictly for now. Relax if this proves to be an issue.
    * Add google-beta provider. I'll need it in the sandbox sooner or later.
    * Remove admin service account creation and all credential-related shenanigans from seed. Will use the identity that is set in ADC and bootstrap it outside of this module.
    * Made flow logs disabled by default (cost control)
    * Enable NAT not just for nodes, but also for pods and services.
    * Better naming convention for NAT resources - routers really are not just about NAT.
    * Move outputs to their own file.
    * Move all input variable definitions into their own file as well.
    * Massive number of updates to cluster resource due to switching to Autopilot

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -2,30 +2,50 @@
 # Manual edits may be lost in future updates.
 
 provider "registry.terraform.io/hashicorp/google" {
-  version     = "4.42.1"
-  constraints = "~> 4.42.1"
+  version     = "4.78.0"
+  constraints = "4.78.0"
   hashes = [
-    "h1:U7SsFbO2wrxPiHd4V2CR90sYXgreQj4fv+rF+I+8aeQ=",
-    "zh:0a4a244b2b65c4e403ae3825d610949cf98c2593a6f6894c36ccd21f5dea7b1e",
-    "zh:0fc5dfaa24d1c790fd17eafbe768d3f8a15b450d80cf3d7ab18f3f81d521adb6",
-    "zh:37fe844eac11ee61da141fdc8502ecec9f722a828754dff7795499de5981de22",
-    "zh:3cacce3c045b19e9ebe985d3de939f0a281cdd8b20cce0a212b48593c172a16a",
-    "zh:429048016c8227e63bb325e6fd78b8f9abc72cf4257b7cd7cba6d950a8601f69",
-    "zh:5a183bace4306b2c3e678dcaa37fa65dd2b5cbcbccf4a859611afac827fd2524",
-    "zh:5c2b99af20eb931321b2b95eff9dbc56ba87744d1047120beb2a62a722baa98f",
-    "zh:87d39b32f6448adb4e17758794c25ecebd19f1889a1634d0b57391a24908a4ef",
-    "zh:8c787a08ff05c67e4182b248943143bcaee8568e4d88562ff7982f711a6664cf",
-    "zh:9dcdda7993d7bf21e660cb521f61a72f3ec5bf5c52f7f25502e4c7648b1f885e",
-    "zh:b122b5a9a521ee96636e517c41c4ead0bf0446df59657b5a136c8a9081449a96",
+    "h1:SGwfLgQ7zDlGeZKn92QUn5zWVhFqO4LvHPTo3VqVq+0=",
+    "zh:09a09e79ac404ea9ce2030185973130ed5f25e7f2c1d46093ee67fcc8f94e220",
+    "zh:1dd579d1200fd9cb57b84b326f401674cc5c62670c85fff7bb90642fd2379d10",
+    "zh:2d03592a8b370c8409fdffb8b946cc1ce9bca7407b48208743adf0de4a65c20c",
+    "zh:5f29e3155b6d0378d30574ac0f1c21aeedbca851e76c26de69ada81987514345",
+    "zh:75da3bb0ee4d4cf45c578462442e757596c2633e1b12afc0ecaaaed30aed25f5",
+    "zh:78b81c837322d66730c9ca04a06580477bfefb8492277cd653874adf5a8a63dc",
+    "zh:895433b46bd1cb1a599641fc0a619a2b6f59166701e923bf56fa6f46dc6d82ad",
+    "zh:9a734177d6c4223d9d65ca80c12bbb1f594b8e97217df6c4f05aa5250e968654",
+    "zh:cd94c7dd81fa777e0552adbcf043951b44e62bcde6eee391aa4d92c67c371c4a",
+    "zh:ceb97e8d2f7c5687c8591f5f8226b3233cf7b728eebb612b5d916d2af6fbf988",
+    "zh:d8ff449fbf8bf317c57b1c9de4c7d15429d52d9aabc048ea7570c9b3ae9810a2",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/google-beta" {
+  version     = "4.78.0"
+  constraints = "4.78.0"
+  hashes = [
+    "h1:4hg6dil0sNudIHTaTWTTTTNV82oT5U2/uTBw48qsprY=",
+    "zh:2c5127ffbc2cd8d9fd47a8148ec9ae0e191591630149503ffa7c73d99069969d",
+    "zh:3b00bb6036e30d6bea36ca69e7154853c62c32493c895f5785b17487003733eb",
+    "zh:61355ac08b2105d534c2462a0edea4d4814ee8da58a3140a444576a63ea3503e",
+    "zh:83509a6db2d272e820f802e0ed04f8bc7768618b31462ec77143f9e53fe24160",
+    "zh:a9fe0cf78ba3348af76dbf6f4281fa3c160d8379238be375b34d8207744935db",
+    "zh:c5b556b93c87bbfe118121fd7a9ca0e3951d1544556fe6813f55ce958998b7d8",
+    "zh:d17dbe6437de81f4bb28684e5ee9d0af55cb87f2b52a82a765894e1d140ec833",
+    "zh:d46abd2269704cddbbdf98b79c686adc4d42b9ad0b02de9e6b641e623ead70c2",
+    "zh:dac11e45a083008cd1e56c2816452b28d7f02d31a9ef114b5157a8e6c642877c",
+    "zh:e37bbc72abbd1a51b3d26ab16c7d7f1969facaa6b6a11c98e2108035bc8cbf43",
+    "zh:ec3f17e794d877d03f1a949978cf351bab48efdbab2613cfe7a9858acee77822",
     "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
   ]
 }
 
 provider "registry.terraform.io/hashicorp/time" {
   version     = "0.9.1"
-  constraints = "~> 0.9.1"
+  constraints = "0.9.1"
   hashes = [
-    "h1:VxyoYYOCaJGDmLz4TruZQTSfQhvwEcMxvcKclWdnpbs=",
+    "h1:NUv/YtEytDQncBQ2mTxnUZEy/rmDlPYmE9h2iokR0vk=",
     "zh:00a1476ecf18c735cc08e27bfa835c33f8ac8fa6fa746b01cd3bcbad8ca84f7f",
     "zh:3007f8fc4a4f8614c43e8ef1d4b0c773a5de1dcac50e701d8abc9fdc8fcb6bf5",
     "zh:5f79d0730fdec8cb148b277de3f00485eff3e9cf1ff47fb715b1c969e5bbd9d4",
--- 010-seed.tf
+++ 010-seed.tf
@@ -1,67 +1,33 @@
-# The resources described in this file use a "seed" Google provider and are deployed
-# with operator's credentials. The purpose of these resources is to provide a foundation
-# to deploy the cluster using a locked-down user-managed service account configured here.
+# For now, assume the target project already exists.
+# TODO document the bootstrap: create a project, add a service account, run Terraform with impersonation.
+# TODO reimplement project creation (using two provider configurations) later
 
-# Pre-requisite: "Cloud Resource Manager API" (cloudresourcemanager.googleapis.com) must be enabled
-# for Terraform to be able to manage IAM policies at project level.
-
-# Pre-requisite: "Service Usage API" (serviceusage.googleapis.com) must be enabled 
-# for Terraform to be able to manage services in a Google Cloud project.
-
-# This module deploys all its resources into a single Google Cloud project,
-# and into a single region. These input variables define that project and region,
-# as well as the default zone in that region. These values are also used 
-# at a Google Terraform provider level as a fallback.
-variable "project" {
-  description = <<-EOT
-    All resources deployed by this module are contained in a single project that must already exist. 
-    The project must be linked to a billing account. 
-    The operator must have enough permissions to:
-      - enable services;
-      - create service accounts;
-      - set IAM policy at project level.
-  EOT
-  type        = string
-  nullable    = false
-}
-variable "region" {
-  description = "The Google Cloud region ID to use as a default with Google Cloud provider."
-  type        = string
-  default     = "europe-west2"
-  nullable    = false
-}
-locals {
-  zone = "${var.region}-a"
-}
-# variable "zone" {
-#   description = "The Google Cloud zone ID to use as a default with Google Cloud provider."
-#   type        = string
-#   default     = "europe-west2-a"
-#   nullable    = false
-#   validation {
-#     condition = startswith(var.zone, var.region)
-#     error_message = "The 'zone' value must be a Google Cloud zone ID located in the region defined by the 'region' value."
-#   }
-# }
+# The seed stage verifies that the project exists and enables required services.
 
 provider "google" {
-  alias   = "seed"
   project = var.project
   region  = var.region
-  zone    = local.zone
-  scopes = [
-    "https://www.googleapis.com/auth/cloud-platform",
-    "https://www.googleapis.com/auth/userinfo.email",
-  ]
 }
 
-# ADR: I sometimes have to repurpose existing Google Cloud projects. 
-# In such use cases, it's not wise to disable the core services, 
-# even when the current infrastructure is being taken down. Thus, 
-# I enable all required services when deploying this module, but
-# on destroy only some services are taken down.
+provider "google-beta" {
+  project = var.project
+  region  = var.region
+}
 
 locals {
+  # Pre-requisite services:
+  #
+  # Cloud Resource Manager API (cloudresourcemanager.googleapis.com)
+  # For: to manage IAM policy for the project
+  #
+  # Service Usage API (serviceusage.googleapis.com)
+  # For: to enable necessary Google Cloud services for the project (?)
+  #
+  # IAM and Service Account Credentials APIs (iam.googleapis.com, iamcredentials.googleapis.com)
+  # For: service account impersonation (?)
+  #
+  # ...
+  # TODO sort alphabetically
   enabled_services = [
     "cloudresourcemanager.googleapis.com",
     "serviceusage.googleapis.com",
@@ -70,152 +36,25 @@ locals {
     "logging.googleapis.com",
     "monitoring.googleapis.com",
     "container.googleapis.com",
+    "dns.googleapis.com",
+    "artifactregistry.googleapis.com",
   ]
 }
 
-# ADR: After extensive research and deliberations, I have made a decision
-# not to disable services on 'destroy'. This is for two reasons.
-# First, when you enable a service, Google Cloud may (and does) enable
-# other services that your service depends on. The list of service dependencies
-# is not public though. This is weird, but we have what we have. 
-# Second, in my opinion the whole process of enabling and disabling services
-# does not add value from the application developer's perspective.
-# The services required by the application must be enabled, but once they are
-# there is no value in disabling them when the application infrastructure 
-# is deprovisioned.
+data "google_project" "default" {
+  provider   = google
+  project_id = var.project
+}
+
+# In long-lived projects I can leave the services on between experiments. In short-lived projects
+# it would not matter because the project will be destroyed. So 'false' is safe.
 resource "google_project_service" "enabled" {
-  provider = google.seed
-  for_each = toset(local.enabled_services)
-  service  = each.key
-
-  # (Optional) If true, services that are enabled and which depend on this service should also be disabled
-  # when this service is destroyed. If false or unset, an error will be generated if any enabled services
-  # depend on this service when destroying it.
+  provider                   = google
+  project                    = data.google_project.default.project_id
+  for_each                   = toset(local.enabled_services)
+  service                    = each.key
   disable_dependent_services = false
-
-  # (Optional) If true, disable the service when the Terraform resource is destroyed. Defaults to true.
-  # May be useful in the event that a project is long-lived but the infrastructure running in that project changes frequently.
-  disable_on_destroy = false
-}
-
-# In production environments, resources usually are deployed by 
-# a locked-down "master" service account and use of "Owner" and 
-# "Editor" basic roles is discouraged. 
-#
-# In this module, 'admin-robot' is the name we give to that service account.
-#
-# The service account will manage the cluster infrastructure, but not Kubernetes objects. 
-# The "Kubernetes Engine Cluster Admin" role contains enough permissions for this use case.
-# A more general "Kubernetes Engine Admin" role would have been excessive.
-# Predefined GKE Roles: https://cloud.google.com/kubernetes-engine/docs/how-to/iam#predefined
-#
-# The service account will also create and manage the service accounts for the cluster nodes.
-# For this, "Service Account Admin" role is required.
-# Reference: https://cloud.google.com/iam/docs/understanding-roles#service-accounts-roles
-#
-# The admin robot will be managing the permissions for the service accounts that it creates for GKE nodes. 
-# This is why it needs "Project IAM Admin" role: 
-# Reference: https://cloud.google.com/resource-manager/docs/access-control-proj#resourcemanager.projectIamAdmin
-locals {
-  admin_robot_roles = [
-    "roles/container.clusterAdmin",
-    "roles/iam.serviceAccountAdmin",
-    "roles/resourcemanager.projectIamAdmin",
-  ]
-}
-resource "google_service_account" "admin_robot" {
-  provider    = google.seed
-  account_id  = "admin-robot"
-  description = "Managing GKE clusters, node service accounts, and their permissions"
-
-  # This module sometimes is deployed in repurposed (long-life) projects, so it has to be
-  # mindful of order of operations when destroying resources. Without declaring an explicit
-  # dependency, Terraform will not know that services must not be disabled until the resources
-  # that rely on them are destroyed. Logically, the 'admin-robot' service account is 
-  # the first resource that is deployed after all required services are enabled, so it makes
-  # to declare all services as its dependencies. In the absence of the following declaration,
-  # creating resources would work as expected, but destroying resources may not because 
-  # without knowing that the service account depends on these services, Terraform will 
-  # start parallel operations and may disable the services before other resources are destroyed.
-  depends_on = [
-    google_project_service.enabled,
-  ]
-}
-resource "google_project_iam_member" "admin_robot" {
-  provider = google.seed
-  project  = var.project
-  for_each = toset(local.admin_robot_roles)
-  role     = each.key
-  member   = google_service_account.admin_robot.member
-}
-
-# To allow the operator (human user) to impersonate 'admin-robot' service account, the operator 
-# must be granted 'Service Account Token Creator' role on the service account.
-# Reference: https://cloud.google.com/iam/docs/impersonating-service-accounts#allow-impersonation
-# Note, that the operator is assumed to be a human user and not a service account.
-# This is fine for my use case.
-
-# The operator identity will be read from this data source.
-data "google_client_openid_userinfo" "operator" {
-  provider = google.seed
-}
-resource "google_service_account_iam_member" "operator_as_admin_robot" {
-  provider           = google.seed
-  service_account_id = google_service_account.admin_robot.name
-  role               = "roles/iam.serviceAccountTokenCreator"
-  member             = "user:${data.google_client_openid_userinfo.operator.email}"
-}
-
-# The operator identity is read from ADC so it may turn out to be not what the operator expects.
-# To follow the principle of least surprise, output (assumed) operator identity.
-output "my-email" {
-  value = data.google_client_openid_userinfo.operator.email
-}
-
-# Google Cloud IAM is eventually consistent and I don't like having to deal with transient permission errors.  
-# A well-known workaround is to wait some reasonable amount of time for IAM to propagate changes.
-# This approach is not infallible because IAM changes may take hours to propagate in the worst case scenario.
-# But it's better than nothing and, empirically, the following delay is enough.
-# FIXME: at some point in the future Terraform should learn to handle eventual consistency gracefully...
-resource "time_sleep" "iam_sync_admin_robot" {
-  create_duration = "120s"
-  depends_on = [
-    google_project_iam_member.admin_robot,
-    google_service_account_iam_member.operator_as_admin_robot,
-  ]
-}
-
-# An access token is required to impersonate a service account.
-# Now that the operator has 'Service Account Token Creator' role on the service account,
-# the token can be requested via the following data source. 
-data "google_service_account_access_token" "operator_as_admin_robot" {
-  provider               = google.seed
-  target_service_account = google_service_account.admin_robot.email
-  scopes = [
-    "userinfo-email",
-    "cloud-platform",
-  ]
-  lifetime = "1200s"
-
-  # The operator was granted a permission to create a token to impersonate 
-  # the 'admin-robot' service account but that IAM change may not have
-  # propagated by the time this data resource is read. To reduce this risk,
-  # introduce a pause between IAM policy change and data source read.
-  depends_on = [
-    time_sleep.iam_sync_admin_robot,
-  ]
-}
-
-# With an access token in hand, the 'admin-robot' service account can be used
-# to deploy the cluster infrastructure. From this point onwards, the 'admin-robot'
-# identity is used for managing the cluster's infrastructure.
-# To ensure this, I create a new instance of Google Cloud Terraform provider.
-# There is no alias set for this instance and thus it becomes the "default" instance.
-provider "google" {
-  project      = var.project
-  region       = var.region
-  zone         = local.zone
-  access_token = data.google_service_account_access_token.operator_as_admin_robot.access_token
+  disable_on_destroy         = false
 }
 
 # This concludes the "seed" stage.
