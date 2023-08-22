b6087a93dd7d74408040515d0eed673dad0faca8
Oliver Frolovs <oliver@devilmicelabs.com>
2023-08-22 14:02:31 +0100

    gitignore: tfvars

--- 010-seed.tf
+++ 010-seed.tf
@@ -1,60 +1,49 @@
-# For now, assume the target project already exists.
-# TODO document the bootstrap: create a project, add a service account, run Terraform with impersonation.
-# TODO reimplement project creation (using two provider configurations) later
-
-# The seed stage verifies that the project exists and enables required services.
+# The seed configuration sets up the provider and enables services on the existing target project.
+# TODO document the bootstrap process: create a project, add a service account, run Terraform with impersonation.
+# TODO add an option to create a new project (using two provider configurations)
 
 provider "google" {
-  project = var.project
-  region  = var.region
+  project = var.google_project
+  region  = var.google_region
 }
 
 provider "google-beta" {
-  project = var.project
-  region  = var.region
+  project = var.google_project
+  region  = var.google_region
 }
 
 locals {
-  # Pre-requisite services:
-  #
-  # Cloud Resource Manager API (cloudresourcemanager.googleapis.com)
-  # For: to manage IAM policy for the project
-  #
-  # Service Usage API (serviceusage.googleapis.com)
-  # For: to enable necessary Google Cloud services for the project (?)
-  #
-  # IAM and Service Account Credentials APIs (iam.googleapis.com, iamcredentials.googleapis.com)
-  # For: service account impersonation (?)
-  #
-  # ...
-  # TODO sort alphabetically
-  enabled_services = [
+  # The services (APIs) to enable on the project, including those provided by the user.
+  enable_services = distinct(compact(concat([
+    "artifactregistry.googleapis.com",
     "cloudresourcemanager.googleapis.com",
-    "serviceusage.googleapis.com",
+    "container.googleapis.com",
+    "dns.googleapis.com",
     "iam.googleapis.com",
     "iamcredentials.googleapis.com",
     "logging.googleapis.com",
     "monitoring.googleapis.com",
-    "container.googleapis.com",
-    "dns.googleapis.com",
-    "artifactregistry.googleapis.com",
-  ]
+    "serviceusage.googleapis.com",
+  ], var.enable_services)))
 }
 
+# Used by other resources to reference the target project by its ID.
+# The existence of the project is checked at 'plan' stage.
 data "google_project" "default" {
   provider   = google
-  project_id = var.project
+  project_id = var.google_project
 }
 
-# In long-lived projects I can leave the services on between experiments. In short-lived projects
-# it would not matter because the project will be destroyed. So 'false' is safe.
+# Enable necessary services (Google Cloud APIs)
 resource "google_project_service" "enabled" {
-  provider                   = google
-  project                    = data.google_project.default.project_id
-  for_each                   = toset(local.enabled_services)
-  service                    = each.key
+  provider = google
+  for_each = toset(local.enable_services)
+  project  = data.google_project.default.project_id
+  service  = each.key
+
+  # In long-lived projects: ok to leave the services on between the deployments.
+  # In short-lived projects: does not matter what these settings are.
+  # Thus, 'false' for both is the safest option.
   disable_dependent_services = false
   disable_on_destroy         = false
 }
-
-# This concludes the "seed" stage.
